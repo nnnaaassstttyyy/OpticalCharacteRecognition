d4f28d05d45ff33b86796f97893df1195e27a9ec
nnnaaassstttyyy <103504483+nnnaaassstttyyy@users.noreply.github.com>
2024-10-06 01:57:25 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,151 +1,69 @@
 import streamlit as st
-import pandas as pd
-import math
-from pathlib import Path
+import pytesseract
+from PIL import Image
+import re
+from fuzzywuzzy import fuzz
 
-# Set the title and favicon that appear in the Browser's tab bar.
-st.set_page_config(
-    page_title='GDP dashboard',
-    page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
-)
+pytesseract.pytesseract.tesseract_cmd = '/opt/homebrew/bin/tesseract'
 
-# -----------------------------------------------------------------------------
-# Declare some useful functions.
+st.title("Распознавание текста с изображения")
 
-@st.cache_data
-def get_gdp_data():
-    """Grab GDP data from a CSV file.
-
-    This uses caching to avoid having to read the file every time. If we were
-    reading from an HTTP endpoint instead of a file, it's a good idea to set
-    a maximum age to the cache with the TTL argument: @st.cache_data(ttl='1d')
-    """
-
-    # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
-    DATA_FILENAME = Path(__file__).parent/'data/gdp_data.csv'
-    raw_gdp_df = pd.read_csv(DATA_FILENAME)
-
-    MIN_YEAR = 1960
-    MAX_YEAR = 2022
-
-    # The data above has columns like:
-    # - Country Name
-    # - Country Code
-    # - [Stuff I don't care about]
-    # - GDP for 1960
-    # - GDP for 1961
-    # - GDP for 1962
-    # - ...
-    # - GDP for 2022
-    #
-    # ...but I want this instead:
-    # - Country Name
-    # - Country Code
-    # - Year
-    # - GDP
-    #
-    # So let's pivot all those year-columns into two: Year and GDP
-    gdp_df = raw_gdp_df.melt(
-        ['Country Code'],
-        [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
-        'Year',
-        'GDP',
-    )
-
-    # Convert years from string to integers
-    gdp_df['Year'] = pd.to_numeric(gdp_df['Year'])
-
-    return gdp_df
-
-gdp_df = get_gdp_data()
-
-# -----------------------------------------------------------------------------
-# Draw the actual page
-
-# Set the title that appears at the top of the page.
-'''
-# :earth_americas: GDP dashboard
-
-Browse GDP data from the [World Bank Open Data](https://data.worldbank.org/) website. As you'll
-notice, the data only goes to 2022 right now, and datapoints for certain years are often missing.
-But it's otherwise a great (and did I mention _free_?) source of data.
-'''
-
-# Add some spacing
-''
-''
-
-min_value = gdp_df['Year'].min()
-max_value = gdp_df['Year'].max()
-
-from_year, to_year = st.slider(
-    'Which years are you interested in?',
-    min_value=min_value,
-    max_value=max_value,
-    value=[min_value, max_value])
-
-countries = gdp_df['Country Code'].unique()
-
-if not len(countries):
-    st.warning("Select at least one country")
-
-selected_countries = st.multiselect(
-    'Which countries would you like to view?',
-    countries,
-    ['DEU', 'FRA', 'GBR', 'BRA', 'MEX', 'JPN'])
-
-''
-''
-''
-
-# Filter the data
-filtered_gdp_df = gdp_df[
-    (gdp_df['Country Code'].isin(selected_countries))
-    & (gdp_df['Year'] <= to_year)
-    & (from_year <= gdp_df['Year'])
-]
-
-st.header('GDP over time', divider='gray')
-
-''
-
-st.line_chart(
-    filtered_gdp_df,
-    x='Year',
-    y='GDP',
-    color='Country Code',
-)
-
-''
-''
+uploaded_file = st.file_uploader("Загрузите изображение", type=["png", "jpg", "jpeg"])
 
 
-first_year = gdp_df[gdp_df['Year'] == from_year]
-last_year = gdp_df[gdp_df['Year'] == to_year]
+def extract_fio(text):
+ 
+    pattern = r'([А-ЯЁ][а-яё]+)\s+([А-ЯЁ][а-яё]+)\s+([А-ЯЁ][а-яё]+)'
+    matches = re.findall(pattern, text)
+    
+    unique_fio = []
+    
+    def is_similar(new_fio, existing_fio):
+        return fuzz.ratio(new_fio, existing_fio) >= 90  
 
-st.header(f'GDP in {to_year}', divider='gray')
+    for fio in matches:
+        fio_str = " ".join(fio)
+        if not any(is_similar(fio_str, existing) for existing in unique_fio):
+            unique_fio.append(fio_str)
+    
+    return unique_fio
 
-''
 
-cols = st.columns(4)
+def extract_birth_dates(text):
 
-for i, country in enumerate(selected_countries):
-    col = cols[i % len(cols)]
+    date_pattern = r'(\d{1,2}[./-]\d{1,2}[./-]\d{2,4})'
+    dates = re.findall(date_pattern, text)
+    
+    unique_dates = set(dates)
+    
+    return unique_dates
 
-    with col:
-        first_gdp = first_year[first_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-        last_gdp = last_year[last_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
 
-        if math.isnan(first_gdp):
-            growth = 'n/a'
-            delta_color = 'off'
+if uploaded_file is not None:
+    
+    image = Image.open(uploaded_file)
+    
+    st.image(image, caption="Загруженное изображение", use_column_width=True)
+    
+
+    if st.button("Распознать текст"):
+        
+        text = pytesseract.image_to_string(image, lang='rus')
+        
+        fio_list = extract_fio(text)
+        
+        if fio_list:
+            st.subheader("Найденные ФИО:")
+            for fio in fio_list:
+                st.text(fio)  
         else:
-            growth = f'{last_gdp / first_gdp:,.2f}x'
-            delta_color = 'normal'
+            st.text("ФИО не найдено.")
 
-        st.metric(
-            label=f'{country} GDP',
-            value=f'{last_gdp:,.0f}B',
-            delta=growth,
-            delta_color=delta_color
-        )
+        birth_dates = extract_birth_dates(text)
+        
+        if birth_dates:
+            st.subheader("Найденные даты рождения:")
+            for date in birth_dates:
+                st.text(date)  
+        else:
+            st.text("Дата рождения не найдена.")
